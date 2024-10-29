ed9db0cec7c06bd38a6dbb7257da45144e6793af
GuDoYoon <rhehfl418q@gmail.com>
2024-10-29 10:50:00 +0900

    ⚙ Setting(#1): tanstack query devtools, router , prettier

--- src/route/Router.tsx
+++ src/route/Router.tsx
@@ -1,1 +1,10 @@
-export default function Router() {}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
