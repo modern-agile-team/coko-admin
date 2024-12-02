c53f1b7e7d58b0a47139277aefe1ac5cc107c044
GuDoYoon <rhehfl418q@gmail.com>
2024-12-02 14:45:01 +0900

    ⚙ Setting(#21): vite.confg수정, 플러그인추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
+    https: true,
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
 });
